3f74f572637cf107772e312330a726131ef25150
ynasiba2024 <t7979176@gmail.com>
2025-11-10 18:17:44 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+# 1-misol
+name = str(input("Ismingizni kiriting: "))
+print(f"Matn uzunligi: {len(name)}")
+
+#2-misol
+matn = "ajksgfjsbkcdbhirhutgfuhbcoajdsvnaouhgoqugdbvfoajsbvofgyosdjbasojfgwehaoufgdjfds"
+print(matn)
+print(f'birinchi harf: {matn [0]}')
+print(f'oxirgi harf: {matn [-1]}')
+
+#3-misol
+matn = +
+(input("Matn kiriting: "))
+print(matn)
+print(matn.upper())
+
+#4-misol
+matn = (input("matn kirit:"))
+print(matn)
+print(matn.lower())
+
+#5-misol
+matn = (input("matn kirit: "))
+print(matn)
+print(matn.strip())
